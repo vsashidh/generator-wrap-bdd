74528014022f729ae1bbd456f2c885b28d9cbeaa
Vivek Sashidharan <vsashidharan@foundationmedicine.com>
2016-02-19 12:17:16 -0500

    modifying example step file to include 2 additional steps

--- app/templates/features/step_definitions/EULA_step_definition.js
+++ app/templates/features/step_definitions/EULA_step_definition.js
@@ -1,6 +1,12 @@
 module.exports = function () {
 
-  this.When(/^"([^"]*)" signs in with password "([^"]*)"$/, {timeout: 100 * 1000}, function (arg1, arg2, callback) {  
+  this.When(/^"([^"]*)" signs in with password "([^"]*)"$/, {timeout: 100 * 1000}, function (arg1, arg2, callback) {
+     // deletes the user role from the backend
+     this.call('C:\\Workspace\\Cheetah\\PathologistPortal\\FMI.Cheetah.Automation\\Web\\FMI-PathologyPortal-Suite.pjs',
+      'FMI-PathologyPortal-Project',
+      'Role.deleteRoleByUserName(' + arg1 + ',Pathologist)',
+      callback);  
+     
      this.call('C:\\Workspace\\Cheetah\\PathologistPortal\\FMI.Cheetah.Automation\\Web\\FMI-PathologyPortal-Suite.pjs',
       'FMI-PathologyPortal-Project',
       'EULA_Verifications.stepEulaSignIn(' + arg1 + ',' + arg2 +')',
@@ -22,7 +28,9 @@ module.exports = function () {
   });
 
   this.Then(/^Worklist page is displayed$/, {timeout: 100 * 1000}, function (callback) {
-    // Write code here that turns the phrase above into concrete actions
-    callback.pending();
+     this.call('C:\\Workspace\\Cheetah\\PathologistPortal\\FMI.Cheetah.Automation\\Web\\FMI-PathologyPortal-Suite.pjs',
+      'FMI-PathologyPortal-Project',
+      'EULA_Verifications.testWorklistPage()',
+      callback);
   });
 };
